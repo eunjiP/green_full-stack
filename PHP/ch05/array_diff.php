cb6c9646f1053238e932441eb77d6cb9de443c9b
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-04-11 17:48:24 +0900

    20220411 수업 내용

---
+++ PHP/ch05/array_diff.php
@@ -0,0 +1,19 @@
+<?php
+    //array_diff 배열 중 다른 값만 출력
+    $arr1 = [1, 2, 3];
+    $arr2 = [1, 2, 3, 4, 5];
+
+    $arr3 = [1, 2, 3];
+
+    $diff_arr = array_diff($arr1,$arr2);
+
+    print_r($diff_arr);
+    print "<br>";
+
+    $diff_arr2 = array_diff($arr2, $arr1);
+    print_r($diff_arr2);
+    print "<br>";
+
+    print ($arr1 === $arr2) . "<br>";
+    print ($arr1 === $arr3) . "<br>";
+?>
