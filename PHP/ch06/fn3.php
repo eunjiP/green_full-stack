5d7beafe3ee6454923d4b2cf19d9a37358f6d598
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-04-14 18:13:46 +0900

    20220414 수업정리

--- PHP/ch06/fn3.php
+++ PHP/ch06/fn3.php
@@ -2,13 +2,13 @@
     $start_num = 1;
     $end_num = 100;
 
-    $result = sum_from_to($start_num, $end_num);
+    $result = sum_from_to($start_num, $end_num);    //아규먼트, 인자, 인수(괄호 안의 값)
 
     print "$start_num ~ $end_num 을(를) 모두 더한 값은 $result 입니다.<br>";
 
     //문제)함수를 정의하세요
     //내답(답 동일)
-    function sum_from_to($num1, $num2)
+    function sum_from_to($num1, $num2)  //파라미터, 매개변수(괄호 안의 값)
     {
         $hap = 0;
         for($i = $num1; $i <= $num2; $i++)
@@ -18,4 +18,5 @@
         return $hap;
     }
 
+    
 ?>
--- PHP/ch06/recursion_function.php
+++ PHP/ch06/recursion_function.php
@@ -21,4 +21,17 @@
     $result = factorial_rec($num); //3 x 2 x 1
     print "${num}!= $result <br>";
 
+    //문제) 절대값 만들기
+    function my_abs($num)
+    {
+        if($num < 0)
+        {
+            $num *= -1;
+            return $num;        //return -$num; 두줄을 한줄로 표현가능
+        }
+        return $num;            //return $num < 0? -$num : $num;  삼항식 표현
+    }
+
+    print "my_abs(-3) : " . my_abs(-3) . "<br>";    //3이 출력되는지 확인
+    print "my_abs(3) : " . my_abs(3) . "<br>";    //3이 출력되는지 확인
 ?>
