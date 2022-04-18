7b2c6dd20e8274d562eeb68fd7eda45473074da0
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-04-18 13:39:25 +0900

    혼자서 구현해보기 연습 -6번째

---
+++ PHP/board6/write_proc.php
@@ -0,0 +1,12 @@
+<?php
+    include "db.php";
+
+    $title = $_POST['title'];
+    $ctnt = $_POST['ctnt'];
+    $conn = get_conn();
+    $sql = "INSERT INTO t_board (title, ctnt) VALUES ('$title', '$ctnt')";
+    $result = mysqli_query($conn, $sql);
+    myslqi_close($conn);
+
+    header('Location: /board6/list.php');
+?>
