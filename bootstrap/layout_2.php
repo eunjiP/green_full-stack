0c20f216ec409d308e4e14465c75b48114cd547f
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-05-23 18:12:54 +0900

    20220523 부트스트랩 수업

---
+++ bootstrap/layout_2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>부트스트랩</title>
+    <style>
+        .container {background-color: palevioletred;}
+        </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <!-- 원래 한줄에 12칸이 존재하는데 col을 생성한 수 만큼 나눠서 들고 간다. 
+                중간의 거터(guttr)까지 이용해서 공간을 맞춰줌(container안에서만 가능) -->
+            <div class="col">
+            Column
+            </div>
+            <div class="col">
+            Column
+            </div>
+            <div class="col">
+            Column
+            </div>
+            <div class="col">
+            Column
+            </div>
+        </div>
+    </div>
+</body>
+</html>
