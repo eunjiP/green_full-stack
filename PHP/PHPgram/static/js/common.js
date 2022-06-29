dfb6a22a54aa0fe7aa50fba40f3e4452754950e1
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-06-29 18:44:15 +0900

    20220629 피드리스트 추가

--- PHP/PHPgram/static/js/common.js
+++ PHP/PHPgram/static/js/common.js
@@ -1,1 +1,31 @@
+//삼항식으로 배열의 키와 값을 쿼리문으로 만들어주는 함수
+//=>로 return을 쓰지 않아도 return됨, join은 나온 배열들을 연결 시켜주는 역할
+function encodeQueryString(params){
+    const keys = Object.keys(params);
+    return keys.length
+            ? "?" + keys.map(key =>
+                        encodeURIComponent(key) + "=" + 
+                        encodeURIComponent(params[key])
+                    ).join("&")
+            : "";
+}
 
+function getDateTimeInfo(dt) {
+    const nowDt = new Date();
+    const targetDt = new Date(dt);
+
+    const nowDtSec = parseInt(nowDt.getTime() * 0.001);
+    const targetDtSec = parseInt(targetDt.getTime() * 0.001);
+
+    const diffSec = nowDtSec - targetDtSec;
+    if(diffSec < 120) {
+        return '1분 전';
+    } else if(diffSec < 3600) { //분 단위 (60 * 60)
+        return `${parseInt(diffSec / 60)}분 전`;
+    } else if(diffSec < 86400) { //시간 단위 (60 * 60 * 24)
+        return `${parseInt(diffSec / 3600)}시간 전`;
+    } else if(diffSec < 2592000) { //일 단위 (60 * 60 * 24 * 30)
+        return `${parseInt(diffSec / 86400)}일 전`;
+    }
+    return targetDt.toLocaleString();
+}
