5046da3833759f3ee5cc4930e503e9924bad3523
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-07-19 13:00:30 +0900

    20220719 오전 수업(상품등록 백엔드 작업, 로그인 후에 상품등록 가능하도록 처리)

--- PHP/PHPShop/application/controllers/ApiController.php
+++ PHP/PHPShop/application/controllers/ApiController.php
@@ -5,4 +5,9 @@ class ApiController extends Controller {
     public function categoryList() {
         return $this->model->getCategoryList();
     }
+
+    public function productInsert() {
+        $json = getJson();
+        return [_RESULT => $this->model->insProduct($json)];
+    }
 }
