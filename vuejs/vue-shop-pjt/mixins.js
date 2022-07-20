69989a325f77c1d3ae5ff3441a262d3b66d8c837
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-07-20 18:18:24 +0900

    20220720 이미지 등록 및 삭제 백앤드, 프론트앤드

--- vuejs/vue-shop-pjt/mixins.js
+++ vuejs/vue-shop-pjt/mixins.js
@@ -11,6 +11,7 @@ export default {
                 console.error(e);
             })).data;
         },
+        
         async $get(url, param) {
             return (await axios.get(url, {
                 params : param
@@ -19,12 +20,23 @@ export default {
             })).data;
         },
 
+        async $delete(url, param) {
+            return (await axios.delete(url, {
+                params : param
+            }).catch(e => {
+                console.log(e);
+            })).data;
+        },
+
         $base64(file) {
+            //프라미스를 사용하여 백엔드에 전달되게 됨 => 순차적으로 처리되는 프라미스로 결과가 나옴
             return new Promise(resolve => {
                 const fr = new FileReader();
                 fr.onload = e => {
+                    //로딩 후 문자열로 된 결과가 담김
                     resolve(e.target.result);
                 }
+                //readAsDataURL : 넘어온 파일을 읽어주는 역할
                 fr.readAsDataURL(file);
             });
         },
--- vuejs/vue-shop-pjt/src/views/ImageInsert.vue
+++ vuejs/vue-shop-pjt/src/views/ImageInsert.vue
@@ -15,9 +15,14 @@
         <label class="col-md-3 col-form-label">섬네일 이미지</label>
         <div class="col-md-9">
           <div class="row">
-
+            <div class="col-lg-3 col-md-4 col-sm-2" :key="item.id" v-for="item in productImage.filter( c => c.type === 1 )">
+              <div class="position-relative">
+                <img :src="`/static/img/${item.product_id}/${item.type}/${item.path}`" class="img-fluid">
+                <div class="position-absolute top-0 end-0" style="cursor:pointer" @click="deleteImage(item.id);">X</div>
+              </div>
+            </div>
           </div>
-          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.file, 1)">
+          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.files, 1)">
           <div class="alert alert-secondary" role="alert">
             <ul>
               <li>이미지 사이즈 : 350 * 350</li>
@@ -32,9 +37,14 @@
         <label class="col-md-3 col-form-label">제품 이미지</label>
         <div class="col-md-9">
           <div class="row">
-          
+           <div class="col-lg-3 col-md-4 col-sm-2" :key="item.id" v-for="item in productImage.filter( c => c.type === 2 )">
+              <div class="position-relative">
+                <img :src="`/static/img/${item.product_id}/${item.type}/${item.path}`" class="img-fluid">
+                <div class="position-absolute top-0 end-0" style="cursor:pointer" @click="deleteImage(item.id);">X</div>
+              </div>
+            </div>
           </div>
-          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.file, 2)">
+          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.files, 2)">
           <div class="alert alert-secondary" role="alert">
             <ul>
               <li>최대 5개 가능</li>
@@ -50,9 +60,14 @@
         <label class="col-md-3 col-form-label">제품설명 이미지</label>
         <div class="col-md-9">
           <div class="row">
-          
+            <div class="col-lg-3 col-md-4 col-sm-2" :key="item.id" v-for="item in productImage.filter( c => c.type === 3 )">
+              <div class="position-relative">
+                <img :src="`/static/img/${item.product_id}/${item.type}/${item.path}`" class="img-fluid">
+                <div class="position-absolute top-0 end-0" style="cursor:pointer" @click="deleteImage(item);">X</div>
+              </div>
+            </div>
           </div>
-          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.file, 3)">
+          <input type="file" class="form-control" accept="image/png, image/jpeg" @change="uploadFile($event.target.files, 3)">
           <div class="alert alert-secondary" role="alert">
             <ul>
               <li>파일 사이즈 : 5Mb 이하</li>
@@ -82,6 +97,7 @@ export default {
   created() {
     this.productId = this.$route.query.product_id; 
     this.productDetail = this.$store.state.sallerSelectedProduct;
+    this.getProductImage();
   },
   methods: {
     /* async getProductDetail() {
@@ -89,6 +105,23 @@ export default {
         `/api/productDetail/${this.productId}`, {});
       console.log(productDetail);
     } */
+    async uploadFile(files, type) {
+      console.log(files);
+      //현재는 이미지 하나만 선택가능해서 0번의 값으로 지정되어 있음
+      const image = await this.$base64(files[0]);
+      //만든 문자열을 객체 안에 넣어 준다
+      const formData = { image };
+      const { error } = await this.$post(`/api/upload/${this.productDetail.id}/${type}`, formData);
+      console.log(error);
+    },
+    async getProductImage() {
+      this.productImage = await this.$get(`/api/productImageList/${this.productDetail.id}`);
+      console.log(this.productImage);
+    },
+    async deleteImage(id) {
+      const result = await this.$delete(`/api/productImageDelete/${id}`);
+      console.log(result);
+    }
   }
 }
 </script>
