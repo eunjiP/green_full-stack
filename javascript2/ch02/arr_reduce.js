201bdef7a7df9409f3cd4c9e09bd7d8db07d731b
eunjiP <101936519+eunjiP@users.noreply.github.com>
2022-07-25 21:40:15 +0900

    주석 추가

--- javascript2/ch02/arr_reduce.js
+++ javascript2/ch02/arr_reduce.js
@@ -1,7 +1,7 @@
 const arr = [11, 22, 33, 44, 55, 66, 77, 88, 99];
 const result = arr.reduce((preVal, curVal) => {
     // reduce : 처음의 preVal에는 arr[0]이 들어가고 그 이후에는 reduce가 리턴한 결과값이 들어간다
-    // curVal는 항상 다음에 올 배열의 값이 온다
+    // curVal는 항상 다음에 올 배열의 값이 온다 => 알고리즘에 많이 사용됨
     return preVal + curVal;
 });
 console.log(`result : ${result}`);
